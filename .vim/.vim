f0fc00c811f95cdf20dc90bf7f6c14c8bc3879c6
Hasan Yusuf Ahmed <hasanyusufahmed@gmal.com>
2018-08-01 01:09:15 -0400

    automated the symlinking with setup.sh

---
+++ .vim/.vim
@@ -0,0 +1,1 @@
+/Users/hasan/.dotfiles/.vim
--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 ln -s $HOME/.dotfiles/.vim $HOME/.vim
-ln -s $HOME/.dotfiles/.vim/.vimrc $HOME/.vimrm
+ln -s $HOME/.dotfiles/.vim/.vimrc $HOME/.vimrc
+ln -s $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
