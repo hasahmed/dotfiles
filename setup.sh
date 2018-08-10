e39f1cac537e8cd4ddbe63584d90e48977711471
Hasan Yusuf Ahmed <hasanyusufahmed@gmal.com>
2018-08-10 00:59:25 -0400

    worked out the submodule thing I think

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+git submodule update --recursive
 ln -sFi $HOME/.dotfiles/.zshrc $HOME/.zshrc
 ln -sFi $HOME/.dotfiles/.vim $HOME/.vim
 ln -sFi $HOME/.dotfiles/.vim/.vimrc $HOME/.vimrc
 ln -sFi $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -sFi $HOME/.dotfiles/oh-my-zsh $HOME/.oh-my-zsh
